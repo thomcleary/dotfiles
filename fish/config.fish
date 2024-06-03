23920be7a2d381ca8d43975846d4d595a7acaaf2
thomcleary <thomasclearydev@gmail.com>
2024-06-03 16:01:20 +0800

    Use starship config for fish syntax highlighting

--- fish/config.fish
+++ fish/config.fish
@@ -5,4 +5,29 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Fish syntax highlighting
+# https://starship.rs/faq/#what-is-the-configuration-used-in-the-demo-gif
+# https://github.com/matchai/dotfiles/blob/b6c6a701d0af8d145a8370288c00bb9f0648b5c2/.config/fish/config.fish
+set -g fish_color_autosuggestion '555'  'brblack'
+set -g fish_color_cancel -r
+set -g fish_color_command --bold
+set -g fish_color_comment red
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_end brmagenta
+set -g fish_color_error brred
+set -g fish_color_escape 'bryellow'  '--bold'
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_match --background=brblue
+set -g fish_color_normal normal
+set -g fish_color_operator bryellow
+set -g fish_color_param cyan
+set -g fish_color_quote yellow
+set -g fish_color_redirection brblue
+set -g fish_color_search_match 'bryellow'  '--background=brblack'
+set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
+
 starship init fish | source
